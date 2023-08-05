518d5a1f1716dd97a8ee85b248f1d680883036f1
hsmulhim <79mulhim@gmail.com>
2023-08-05 23:46:11 +0300

    added the custom appbar

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:hm_1/screens/home_screen.dart';
+import 'package:hm_1/widgets/customAppbar.dart';
 
 
 class loginScreen extends StatelessWidget {
-  const loginScreen({super.key});
+   loginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CustomAppBar(title: 'Login',),
       body: Container(),
 
     );
---
+++ lib/widgets/customAppbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
+   CustomAppBar({super.key, required this.title});
+  final String title;
+@override
+  Size get preferredSize => const Size.fromHeight(100);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(icon: Icon(Icons.arrow_back),color: Colors.black,onPressed: ()=>Navigator.of(context).pop(),),
+      elevation: 0,
+      backgroundColor: Colors.white,
+      title: Text(title,
+      style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+    );
+  }
+}
